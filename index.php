dd37890652fa78e3543e54ca5402666f720b45d1
Chatbot123 <42810864+Chatbot123@users.noreply.github.com>
2018-10-04 11:47:08 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,46 @@
 <?php
+
 $method = $_SERVER['REQUEST_METHOD'];
 //process only when method id post
 if($method == 'POST')
 {
-	
+
 $sessionID = uniqid('',true);
 
-try {
-    // create curl resource
-    $ch = curl_init();
-    // set url
-    curl_setopt($ch, CURLOPT_URL, "https://api.api.ai/v1/query?v=20181001&e=TESTEVENT&lang=en&sessionId=" . $sessionID);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Authorization: Bearer a7fa07b1e8cb46bc881c1a8bd1491838'));
-    //return the transfer as a string
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    // $output contains the output string
-    $output = curl_exec($ch);
-    // close curl resource to free up system resources
-    curl_close($ch);
-}catch (Exception $e) {
-    $speech = $e->getMessage();
-    $fulfillment = new stdClass();
-    $fulfillment->speech = $speech;
-    $result = new stdClass();
-    $result->fulfillment = $fulfillment;
-    $response = new stdClass();
-    $response->result = $result;
-    echo json_encode($response);
+	$event_url = "https://api.dialogflow.com/v1/query?v=20181001";
+	$username    = "intelligentmachine2018@gmail.com";
+    $password    = "Centurylink2018";
+		$ch      = curl_init($event_url);
+    	$options = array(
+        	CURLOPT_SSL_VERIFYPEER => false,
+        	CURLOPT_RETURNTRANSFER => true,
+        	CURLOPT_USERPWD        => "{$username}:{$password}",
+        	CURLOPT_HTTPHEADER     => array( 'Content-Type: application/json',
+						'Authorization: Bearer a7fa07b1e8cb46bc881c1a8bd1491838'
+											),
+			CURLOPT_POSTFIELDS	=> array( "lang": "en",
+							 "query": "I need apples",
+							 "sessionId": "$sessionID",
+							"timezone": "America/New_York",
+							"event": "TESTEVENT"
+			
+			)
+    		);
+    		curl_setopt_array( $ch, $options );
+		$json = curl_exec( $ch );
+		$someobj = json_decode($json,true);
+		$speech = $someobj;
 	
+	$response = new \stdClass();
+    	$response->fulfillmentText = $speech;
+    	$response->source = "webhook";
+	echo json_encode($response);
+
 }
-?>
+else
+{
+	echo "Method not allowed";
+}
+
+
+	
