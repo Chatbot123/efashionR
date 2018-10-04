32936c9b2c59d5aad7b89d99cdea0eebbd99a06d
Chatbot123 <42810864+Chatbot123@users.noreply.github.com>
2018-10-04 11:53:18 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,13 +8,13 @@ if($method == 'POST')
 $sessionID = uniqid('',true);
 
 	$event_url = "https://api.dialogflow.com/v1/query?v=20181001";
-	//$username    = "intelligentmachine2018@gmail.com";
-  //  $password    = "Centurylink2018";
+	$username    = "intelligentmachine2018@gmail.com";
+    $password    = "Centurylink2018";
 		$ch      = curl_init($event_url);
     	$options = array(
         	CURLOPT_SSL_VERIFYPEER => false,
         	CURLOPT_RETURNTRANSFER => true,
-        	//CURLOPT_USERPWD        => "{$username}:{$password}",
+        	CURLOPT_USERPWD        => "{$username}:{$password}",
         	CURLOPT_HTTPHEADER     => array( 'Content-Type: application/json',
 						'Authorization: Bearer a7fa07b1e8cb46bc881c1a8bd1491838'
 											),
@@ -42,5 +42,6 @@ else
 	echo "Method not allowed";
 }
 
+?>
 
 	
