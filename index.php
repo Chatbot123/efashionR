930d25019ac05e40b90e262e5f185583a6124fa2
Chatbot123 <42810864+Chatbot123@users.noreply.github.com>
2018-10-04 12:04:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,30 +7,30 @@ if($method == 'POST')
 
 $sessionID = uniqid('',true);
 
-	$event_url = "https://api.dialogflow.com/v1/query?v=20181001";
-	$username    = "intelligentmachine2018@gmail.com";
-    $password    = "Centurylink2018";
+	$event_url = "https://api.dialogflow.com/v1/query?v=20181001&sessionId=".$sessionID;
+	//$username    = "intelligentmachine2018@gmail.com";
+    //$password    = "Centurylink2018";
 		$ch      = curl_init($event_url);
     	$options = array(
         	CURLOPT_SSL_VERIFYPEER => false,
         	CURLOPT_RETURNTRANSFER => true,
-        	CURLOPT_USERPWD        => "{$username}:{$password}",
+        	//CURLOPT_USERPWD        => "{$username}:{$password}",
         	CURLOPT_HTTPHEADER     => array( 'Content-Type: application/json',
 						'Authorization: Bearer a7fa07b1e8cb46bc881c1a8bd1491838'
 											),
 			CURLOPT_POSTFIELDS	=> array( "lang": "en",
 							 "query": "I need apples",
-							 "sessionId": "$sessionID",
-							"timezone": "America/New_York",
+							 
+							
 							"event": "TESTEVENT"
 			
-			)
+							)
     		);
     		curl_setopt_array( $ch, $options );
 		$json = curl_exec( $ch );
 		$someobj = json_decode($json,true);
 		$speech = $someobj;
-	
+	echo $speech;
 	$response = new \stdClass();
     	$response->fulfillmentText = $speech;
     	$response->source = "webhook";
